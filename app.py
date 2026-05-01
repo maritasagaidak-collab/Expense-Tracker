0cf2e176aadf12bc5dcd606efb36f473049c4a96
Daniil <maritasagaidak@gmail.com>
2026-05-01 20:43:32 +0200

    steps for deploy8

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_login import LoginManager
 from models import User, db
 from auth.routes import auth_bp
 from expenses.routes import expenses_bp
-
+import os
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = 'super-secret-key'
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
@@ -23,12 +23,14 @@ def start():
     return redirect(url_for('auth.login'))
 
 @app.before_request
-def before_request(): db.connect(reuse_if_open=True)
+def before_request():
+    db.connect(reuse_if_open=True)
 
 @app.after_request
-def after_request(res):
+def after_request(response):
     db.close()
-    return res
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
