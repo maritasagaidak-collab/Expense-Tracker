d123c6acfffa738fa10d876b83aa625361489c04
Daniil <maritasagaidak@gmail.com>
2026-05-01 20:35:15 +0200

    steps for deploy7

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from peewee import *
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SqliteDatabase('expenses.db')
+db = SqliteDatabase('expenses_v2.db')
 
 class BaseModel(Model):
     class Meta:
